d5b1cc25516eecf673c7286bcee53ca70cf550ac
Maximilian Graf <max@quest.garden>
2025-04-04 22:28:49 +0200

    feat: add routing and sidebar layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import ManagerView from "./pages/ManagerView";
+import CustomerView from "./pages/CustomerView";
+
 export default function App() {
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Hotel Booking App</h1>
-        </div>
-    )
+        <Router>
+            <div className="flex">
+                <Sidebar />
+                <div className="flex-1 p-4">
+                    <Routes>
+                        <Route path="/" element={<ManagerView />} />
+                        <Route path="/customer" element={<CustomerView />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
---
+++ frontend/src/pages/CustomerView.tsx
@@ -0,0 +1,3 @@
+export default function CustomerView() {
+    return <div className="p-4">🏠 Customer View (Room selection will be here)</div>;
+}
