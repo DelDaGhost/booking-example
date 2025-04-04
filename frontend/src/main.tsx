1ff55ccc34516cd1b0ab665ad559f2cdac2bfdcc
Maximilian Graf <max@quest.garden>
2025-04-04 22:54:48 +0200

    feat(manager): implement manager calendar view for room bookings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    HttpLink,
+} from '@apollo/client'
+
+const httpLink = new HttpLink({
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT, // e.g., http://localhost:4000/graphql
+    fetch,
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
 )
